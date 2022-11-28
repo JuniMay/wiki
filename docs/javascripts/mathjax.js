a748fe2875109b1a1080e5572b1cd2d572d10c34
Juni May <juni_may@outlook.com>
2022-11-28 10:38:24 +0800

    :rocket: update mathjax support

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,19 +1,55 @@
 window.MathJax = {
-    loader: {load: ['[tex]/boldsymbol']},
-    tex: {
-      inlineMath: [["\\(", "\\)"]],
-      displayMath: [["\\[", "\\]"]],
-      processEscapes: true,
-      processEnvironments: true,
-      packages: {'[+]': ['boldsymbol']}
-    },
-    options: {
-      ignoreHtmlClass: ".*|",
-      processHtmlClass: "arithmatex"
-    }
-  };
-  
-  document$.subscribe(() => { 
-    MathJax.typesetPromise()
-  })
-  
+  loader: {
+    load: [
+      "input/tex-base",
+      "output/svg",
+      "ui/menu",
+      "[tex]/require",
+      "[tex]/boldsymbol",
+      "[tex]/ams",
+      "[tex]/verb",
+      "[tex]/color",
+      "[tex]/braket",
+    ],
+  },
+  tex: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+    processEscapes: true,
+    processEnvironments: true,
+    // packages: {'[+]': ['boldsymbol']}
+    packages: [
+      "base",
+      "require",
+      "ams",
+      "boldsymbol",
+      "color",
+      "enclose",
+      "extpfeil",
+      "html",
+      "mhchem",
+      "newcommand",
+      "noerrors",
+      "noundefined",
+      "physics",
+      "unicode",
+      "verb",
+      "xypic",
+      "braket"
+    ],
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
